0f15ff47e7da892f0cec80718c491b0b66f1360a
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-09-07 19:51:01 +0200

    added More

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.AvaloniaUIFactory;
+using NetSparkleUpdater;
 
 namespace Freetime_Wallet;
 
@@ -7,5 +9,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnCheckUpdateClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var sparkle = new SparkleUpdater(
+            new NetSparkleConfiguration("https://github.com/FreetimeMaker/Freetime-Wallet-Desktop/appcast.xml"),
+            new AvaloniaUIFactory()
+        );
+        sparkle.StartLoop(true, true);
     }
 }
